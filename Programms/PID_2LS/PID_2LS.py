6b1a7c7405e5d982206b5bd2787fac260bf1a4a2
TheBaronBlood <69164517+TheBaronBlood@users.noreply.github.com>
2024-01-25 01:31:51 +0100

    Update PID_2LS.py
    Ein Graph wurde hinzugefügt der den Soll- und Istwert anzeigt

--- Programms/PID_2LS/PID_2LS.py
+++ Programms/PID_2LS/PID_2LS.py
@@ -1,23 +1,26 @@
 import color_sensor, motor, runloop
 from hub import port, button
+from app import linegraph
 import asyncio
 
 async def main():
 
+    ############ Hier wegenwir die Regler Werte
+    Kp = 2.2   # ROT
+    Kd = 0.001  # GRÜN
+    Ki = 0.001   
     ###########
-    Kp = 0.9   # Hier wegenwir die Regler Werte
-    Kd = 0.04
-    Ki = 0.00005
-    ###########
-
 
     L = 0
     R = 0
     I = 0
     Error = 0
+    a = 0
+    Setpoint = 0
     Last_Error = 0
-    DriveSpeed = 150
+    DriveSpeed = 200
 
+    linegraph.clear_all()
 
     while not button.pressed(button.LEFT):
 
@@ -28,7 +31,7 @@ async def main():
 
         ########## P I D ############
 
-        Error = R - L
+        Error = R - L # (LÖSUNG: war es einfach von L - R --> R - L)
 
         P = Error
         I = I + Error
@@ -40,6 +43,16 @@ async def main():
 
         motor.run(port.B, int(PIDturn - DriveSpeed))
         motor.run(port.A, int(PIDturn + DriveSpeed))
+
+    
+        a += 1# Inkrementierung der Zählvariable
+
+        linegraph.plot(0, x=a, y=0)
+        linegraph.plot(9, x=a, y=Error)# Plotte den aktuellen PID-Wert        ROT
+        linegraph.plot(3, x=a, y=PIDturn)# Plotte den aktuellen PID-Wert      BLAU
+        linegraph.plot(6, x=a, y=D)# Plotte den aktuellen PID-Wert            GRÜN
+        #await asyncio.sleep(0.1)
+        linegraph.show(False)
 
 
 loop = asyncio.get_event_loop()
